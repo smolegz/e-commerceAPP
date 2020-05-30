7a0ec03c6b36a2c2538e9e7357d54f4d8a52e57a
smolegz <zhuohui.koo@gmail.com>
2020-05-31 03:26:25 +0800

    implemented firebase utils, included ability to store user signup/google sign in into database

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndSignUpPage from "./pages/sign-InOut/sign-InOut.component";
-import { auth } from "./firebase/firebase.utils";
+import {
+	auth,
+	currentUserProfileDocument,
+	createUserProfileDocument,
+} from "./firebase/firebase.utils";
 
 class App extends React.Component {
 	constructor() {
@@ -20,9 +24,22 @@ class App extends React.Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-			this.setState({ currentUser: user });
-			console.log(user);
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+			if (userAuth) {
+				const userRef = await createUserProfileDocument(userAuth);
+
+				userRef.onSnapshot((snapShot) => {
+					this.setState({
+						currentUser: {
+							id: snapShot.id,
+							...snapShot.data(),
+						},
+					});
+					console.log(this.state);
+				});
+			} else {
+				this.setState({ currentUser: userAuth });
+			}
 		});
 	}
 
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,7 +3,7 @@ import "./homepage.styles.scss";
 import Directory from "../../components/directory/directory.component";
 
 const HomePage = (props) => {
-	console.log(props);
+	// console.log(props);
 	return (
 		<div className='homepage'>
 			<Directory />
