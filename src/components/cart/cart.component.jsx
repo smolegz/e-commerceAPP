9e34b5e95ee824222d434a90665e7b0b6e9bfaeb
smolegz <zhuohui.koo@gmail.com>
2020-06-02 23:46:41 +0800

    added cart counter feature

--- src/components/cart/cart.component.jsx
+++ src/components/cart/cart.component.jsx
@@ -1,14 +1,23 @@
 import React from "react";
-
+import { connect } from "react-redux";
 import CustomButton from "../custom-button/custom-button.component";
+import CartItem from "../cart-item/cart-item.component";
 
 import "./cart.styles.scss";
 
-const Cart = () => (
+const Cart = ({ cartItems }) => (
 	<div className='cart-dropdown'>
-		<div className='cart-items' />
+		<div className='cart-items'>
+			{cartItems.map((cartItem) => (
+				<CartItem key={cartItem.id} item={cartItem} />
+			))}
+		</div>
 		<CustomButton>GO TO CHECK OUT</CustomButton>
 	</div>
 );
 
-export default Cart;
+const mapStateToProps = ({ cartReducer: { cartItems } }) => ({
+	cartItems,
+});
+
+export default connect(mapStateToProps)(Cart);
