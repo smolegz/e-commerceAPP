787c2e8d956e6b150babb6f9b56cf971eec10f07
smolegz <zhuohui.koo@gmail.com>
2020-06-02 21:43:27 +0800

    added cart actions which allows items added to cart to be stored in state

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -2,6 +2,7 @@ import Cart from "../../components/cart/cart.component";
 
 const CartActionTypes = {
 	TOGGLE_CART_HIDDEN: "TOGGLE_CART_HIDDEN",
+	ADD_ITEM: "ADD_ITEM",
 };
 
 export default CartActionTypes;
